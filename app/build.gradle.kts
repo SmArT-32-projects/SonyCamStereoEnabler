25a3558e9f6cbb5030a7c66037bb74f12cfd77f0
SmArT-32 <228971816+SmArT-32-projects@users.noreply.github.com>
2025-12-25 00:35:42 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.smart32.sonycamstereoenabler"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.smart32.sonycamstereoenabler"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    compileOnly(files("libs/xposed-api-82.jar"))
+}
